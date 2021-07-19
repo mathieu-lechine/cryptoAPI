6b4fe7e4f2e2c4d00cbc2d131daf937922c9a5be
m.lechine <m.lechine@criteo.com>
2021-07-19 08:58:05 +0200

    Changes to create lambda.zip

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cryptoAPI
 A demo API demonstrating how to create a Python-based REST API with FastAPI, Mangum, Amazon Lambda and API Gateway
+
+## setup python environment
+
+Mark directory v1 as "sources Root" in Pycharm
+
+````bash
+conda create --name cryptoAPI python=3.8
+conda activate cryptoAPI
+cd api && pip install -r requirements.txt
+cd api && uvicorn main:app --reload
+````
+
+
+
--- api/package.bash
+++ api/package.bash
@@ -1,7 +1,8 @@
-root_dir='/Users/anna/Desktop/mediumAPI'
-venv_dir="$root_dir/venv/lib/python3.8/site-packages"
-bucket_name="medium-aws"
-function_name="medium-aws"
+#root_dir='/Users/anna/Desktop/mediumAPI'
+root_dir='/Users/m.lechine/python_projects/cryptoAPI'
+venv_dir='/Users/m.lechine/opt/anaconda3/envs/cryptoAPI'
+bucket_name="matl-cryptoapi"
+function_name="matl-cryptoapi"
 cd $venv_dir && zip -r9 "$root_dir/lambda.zip" . \
 && cd "$root_dir/api" && zip -g ../lambda.zip -r .
 
